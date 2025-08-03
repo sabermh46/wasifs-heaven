25cceffd07d20a8e00956458a4adbe7ed4aba022
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2025-08-03 06:01:15 +0600

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename='/wasifs-heaven'>
           <App />
         </BrowserRouter>
       </PersistGate>
