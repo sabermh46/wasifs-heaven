4a7eef6d6d72438b2332336829805d7b7469fda1
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2025-10-04 21:18:29 +0600

    qwerty

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter, HashRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/wasifs-heaven'>
+        <HashRouter basename='/wasifs-heaven'>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </StrictMode>,
