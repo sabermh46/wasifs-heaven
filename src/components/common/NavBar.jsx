d220cf94c43d0859e1d7bdd450739942a34f3289
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2025-10-04 21:42:45 +0600

    fgjk

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { motherWrapper } from '../../utils/tailcss'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <header className="bg-black/20 backdrop-blur-sm text-white sticky top-0 z-50">
         <nav className={`${motherWrapper} border-b border-slate-500/50 py-3 px-0`}>
-            <div className="brand">
-                WH
-            </div>
+            <Link to="/">Home</Link> |{' '}
+            <Link to="/books">Books</Link> |{' '}
+            <Link to="/admin">Admin</Link>
         </nav>
     </header>
   )
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wasifs-heaven/', 
+  base: '/wasifs-heaven', 
   plugins: [
     react(),
     tailwindcss(),
